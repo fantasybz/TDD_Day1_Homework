00676ca24fc2e85c05c89c737bc654d771ac16ca
fantasybz <fantasybz@KOCHI-MACBOOKPR>
2017-01-12 16:53:59 +0800

    建立測試專案

---
+++ SumProducItemGroup/SumProducItemGroupClassLibraryTests/SumProductUtilityTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SumProducItemGroupClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumProducItemGroupClassLibrary.Tests
+{
+    [TestClass()]
+    public class SumProductUtilityTests
+    {
+        [TestMethod()]
+        public void SumByGroupTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
